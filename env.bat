bf7ad6d86a2e51e4ba60b8d9a746a459360179b8
karata <karata.brown@outlook.com>
2015-01-21 10:33:49 +0800

    Split env variable

---
+++ env.bat
@@ -0,0 +1,5 @@
+set BRANCH1=
+set BRANCH2=
+
+set PATH=%PATH%;C:\Program Files (x86)\MSBuild\12.0\Bin
+set ROOT=%CD%
--- startup.bat
+++ startup.bat
@@ -1,6 +1,6 @@
 @echo off
 doskey /MACROFILE=%~dp0aliases.doskey
-PATH=%PATH%;C:\Program Files (x86)\MSBuild\12.0\Bin
+call %~dp0env.bat
 echo Welcome to OData!
 echo Current dev branch:
 git branch
