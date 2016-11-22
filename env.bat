b2c287096888e708e3182dc32592067e77d2e9e0
Dong Liu <doliu@microsoft.com>
2016-11-22 10:13:21 +0800

    Update alias and env.

--- env.bat
+++ env.bat
@@ -1,5 +1,5 @@
 set BRANCH1=PoliCheck
 set BRANCH2=PoliCheck.Stable
 
-set PATH=%PATH%;C:\Program Files (x86)\MSBuild\12.0\Bin;%~dp0\util
+set PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin;%~dp0\util
 set ROOT=%CD%
